2fe07b9ffd8d6e2a8c0b62974be4271017eec158
aa800902 <oiloiloilroxas@gmail.com>
2017-09-07 16:40:42 +0800

    測試git commit

--- QTProject/QTProject/ChangeParentCommand.cpp
+++ QTProject/QTProject/ChangeParentCommand.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 ChangeParentCommand::ChangeParentCommand(Component *oriNode, Component *oldParent, Component *newParent)
 {
+	// 測試修改
 	this->oriNode = oriNode;
 	this->newParent = newParent;
 	this->oldParent = oldParent;
